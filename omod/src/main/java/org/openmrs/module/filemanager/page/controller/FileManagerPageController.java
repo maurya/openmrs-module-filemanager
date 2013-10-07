c7c9a4493a2aba134bc1227e187a5e23c0a0e881
gitahi86 <gitahi86@gmail.com>
2013-10-07 11:37:32 +0300

    Improvements to FileManagerPageController.

--- omod/src/main/java/org/openmrs/module/filemanager/page/controller/FileManagerPageController.java
+++ omod/src/main/java/org/openmrs/module/filemanager/page/controller/FileManagerPageController.java
@@ -2,17 +2,23 @@ package org.openmrs.module.filemanager.page.controller;
 
 import org.openmrs.Person;
 import org.openmrs.Patient;
+import org.openmrs.Visit;
 import org.openmrs.module.emrapi.patient.PatientDomainWrapper;
+import org.openmrs.module.filemanager.api.FileManagerService;
+import org.openmrs.module.filemanager.api.impl.FileManagerServiceImpl;
+import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.page.PageModel;
 import org.openmrs.ui.framework.annotation.InjectBeans;
 import org.openmrs.ui.framework.page.PageModel;
+import org.openmrs.util.OpenmrsUtil;
 import org.openmrs.web.WebConstants;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
@@ -22,8 +28,8 @@ import java.util.List;
 public class FileManagerPageController {
 
 	public void controller(@RequestParam("patientId") Patient patient,
-                           PageModel model,
-                           @InjectBeans PatientDomainWrapper patientDomainWrapper) {
+						   PageModel model,
+						   @InjectBeans PatientDomainWrapper patientDomainWrapper) {
 
 		List<Person> personList = new ArrayList<Person>();
 		for (int i = 1; i <= 10; i++) {
@@ -35,36 +41,40 @@ public class FileManagerPageController {
 			personList.add(p);
 		}
 
-        patientDomainWrapper.setPatient(patient);
+		patientDomainWrapper.setPatient(patient);
 
-        model.addAttribute("patient", patientDomainWrapper);
-//		model.addAttribute("user", sessionContext.getCurrentUser());
+		model.addAttribute("patient", patientDomainWrapper);
 		model.addAttribute("files", personList);
 	}
 
-    public String post(@RequestParam(value = "patientId", required = false) Patient patient,
-                       @RequestParam(value = "returnUrl", required = false) String returnUrl,
-                       UiUtils ui,
-                       HttpServletRequest request, PageModel model) {
-        if (request instanceof MultipartHttpServletRequest) {
-            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-            MultipartFile file = multipartRequest.getFile("file");
-            if (!file.isEmpty()) {
-                if (file.getSize() <= 5242880) {
-                    try {
-                        FileOutputStream fos = new FileOutputStream("/home/gitahi/aaaaa2.jpg");
-                        fos.write(file.getBytes());
-                        fos.close();
-                    } catch (Exception ex) {
+	public String post(@RequestParam("patientId") Patient patient,
+//					   @RequestParam("visitId") Visit visit,
+					   @RequestParam(value = "returnUrl", required = false) String returnUrl,
+					   HttpServletRequest request,
+					   UiUtils ui) {
+		if (request instanceof MultipartHttpServletRequest) {
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			MultipartFile file = multipartRequest.getFile("file");
+			if (!file.isEmpty()) {
+				File dataDir = OpenmrsUtil.getDirectoryInApplicationDataDirectory("openmrs.module.filemanager");
+				FileManagerService service = new FileManagerServiceImpl();
 
-                    }
-                } else {
-                    request.getSession().setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-                            "feedback.notification.feedback.error");
-                    return null;
-                }
-            }
-        }
-        return null;
-    }
+//				new FileManagerService().saveComplexObs(patient, visit, file, "", "");
+				if (file.getSize() <= 5242880) {
+					try {
+						FileOutputStream fos = new FileOutputStream("/home/gitahi/aaaaa2.jpg");
+						fos.write(file.getBytes());
+						fos.close();
+					} catch (Exception ex) {
+
+					}
+				} else {
+					request.getSession().setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
+							"feedback.notification.feedback.error");
+					return null;
+				}
+			}
+		}
+		return "redirect:" + ui.pageLink("filemanager", "fileManager", SimpleObject.create("patientId", patient.getId(), "returnUrl", returnUrl));
+	}
 }
