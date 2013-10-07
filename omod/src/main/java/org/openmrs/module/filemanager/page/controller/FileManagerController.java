324c37016e112c56b62476651c9bed2797085115
harsha89 <harsz89@gmail.com>
2013-10-07 13:19:49 +0530

    file manager service updated

--- omod/src/main/java/org/openmrs/module/filemanager/page/controller/FileManagerController.java
+++ omod/src/main/java/org/openmrs/module/filemanager/page/controller/FileManagerController.java
@@ -1,12 +1,5 @@
 package org.openmrs.module.filemanager.page.controller;
 
-import org.openmrs.web.WebConstants;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * Created with IntelliJ IDEA.
  * User: harsha
@@ -16,25 +9,7 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class FileManagerController {
 
-    public String post(HttpServletRequest request) {
-        String description=request.getParameter("description");
-        String fileType=request.getParameter("type");
-
-        if (request instanceof MultipartHttpServletRequest) {
-            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-            MultipartFile file             = (MultipartFile) multipartRequest.getFile("file");
-
-            if (!file.isEmpty()) {
-                if (file.getSize() <= 5242880) {
-                    if (file.getOriginalFilename().endsWith(".jpeg") || file.getOriginalFilename().endsWith(".jpg")
-                            || file.getOriginalFilename().endsWith(".gif")
-                            || file.getOriginalFilename().endsWith(".png")) {
-                    } else {
-
-                    }
-                }
-            }
-        }
+    public String controlller() {
         return "redirect:helloworldapp/helloworld.page";
     }
 }
